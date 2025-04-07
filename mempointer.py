5b8a883b3a9d5a477d04dcec21906f270d5e74b5
Dzavoy <dzavoy@proton.me>
2025-04-07 02:34:39 -0300

    added typing

--- mempointer.py
+++ mempointer.py
@@ -1,37 +1,43 @@
+from __future__ import annotations
+from typing import Any
 import re
 import pymem
+from pymem import Pymem
 
 class MemoryPointer:
-    def __init__(self, process, module, address=0):
-        self._process = process
+    def __init__(self, process: str | Pymem, module: str | Any, address: int = 0) -> None:
+        self._process: Pymem
         if isinstance(process, str):
             self._process = pymem.Pymem(process)
+        else: self._process = process
         
-        self._module = module
+        self._module: Any
         if isinstance(module, str):
             self._module = pymem.process.module_from_name(self._process.process_handle, module)
-        
-        self._address = address
+        else:
+            self._module = module
+
+        self._address: int = address
     
-    def relocate_pattern(self, regex_pattern):
-        lookup_bytes = self._process.read_bytes(self._module.lpBaseOfDll, self._module.SizeOfImage)
-        result = re.search(regex_pattern, lookup_bytes)
+    def relocate_pattern(self, regex_pattern: bytes) -> MemoryPointer:
+        lookup_bytes: bytes = self._process.read_bytes(self._module.lpBaseOfDll, self._module.SizeOfImage)
+        result: re.Match | None = re.search(regex_pattern, lookup_bytes)
         if not result:
             raise Exception(f'Failed to find pattern {regex_pattern} in module "{self._module.module_name}".')
         return MemoryPointer(
             self._process, self._module, self._module.lpBaseOfDll + result.start()
         )
 
-    def offset(self, amount):
+    def offset(self, amount: int) -> MemoryPointer:
         return MemoryPointer(
             self._process, self._module, self._address + amount
         )
     
-    def pointer_walk(self, *offsets):
+    def pointer_walk(self, *offsets: int) -> MemoryPointer:
         if not offsets:
             return self
         
-        new_address = self._address + offsets[0]
+        new_address: int = self._address + offsets[0]
 
         for offset in offsets[1:]:
             new_address = self._process.read_ulonglong(new_address)
@@ -41,26 +47,26 @@ class MemoryPointer:
             self._process, self._module, new_address
         )
 
-    def dereference(self):
-        new_address = self._process.read_ulonglong(self._address)
+    def dereference(self) -> MemoryPointer:
+        new_address: int = self._process.read_ulonglong(self._address)
         return MemoryPointer(
             self._process, self._module, new_address
         )
     
-    def read_int(self):
+    def read_int(self) -> int:
         return self._process.read_int(self._address)
     
-    def read_string(self):
+    def read_string(self) -> str:
         return self._process.read_string(self._address)
     
-    def read_float(self):
+    def read_float(self) -> float:
         return self._process.read_float(self._address)
     
-    def read_bytes(self, size):
+    def read_bytes(self, size: int) -> bytes:
         return self._process.read_bytes(self._address, size)
     
-    def hex(self):
+    def hex(self) -> str:
         return hex(self._address)
 
-    def __int__(self):
+    def __int__(self) -> int:
         return self.address
