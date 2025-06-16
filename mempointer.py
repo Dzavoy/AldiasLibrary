1bba5fb69b401ad6de9b3faf67c251c76b3dca57
Dzavoy <dzavoy@proton.me>
2025-06-16 02:39:07 -0300

    added mypy.ini

--- mempointer.py
+++ mempointer.py
@@ -10,8 +10,9 @@ class MemoryPointer:
         self._process: Pymem
         if isinstance(process, str):
             self._process = pymem.Pymem(process)
-        else: self._process = process
-        
+        else:
+            self._process = process
+
         self._module: Any
         if isinstance(module, str):
             self._module = pymem.process.module_from_name(
@@ -21,7 +22,7 @@ class MemoryPointer:
             self._module = module
 
         self._address: int = address
-    
+
     def relocate_pattern(self, regex_pattern: bytes) -> MemoryPointer:
         lookup_bytes: bytes = self._process.read_bytes(
             self._module.lpBaseOfDll, self._module.SizeOfImage
@@ -30,10 +31,10 @@ class MemoryPointer:
         result: re.Match | None = re.search(regex_pattern, lookup_bytes)
         if not result:
             raise Exception(
-                f'Failed to find pattern {regex_pattern} in module'
+                f'Failed to find pattern {regex_pattern.decode("utf-8")} in module'
                 f'"{self._module.module_name}".'
             )
-            
+
         return MemoryPointer(
             self._process, self._module,
             self._module.lpBaseOfDll + result.start()
@@ -43,17 +44,17 @@ class MemoryPointer:
         return MemoryPointer(
             self._process, self._module, self._address + amount
         )
-    
+
     def pointer_walk(self, *offsets: int) -> MemoryPointer:
         if not offsets:
             return self
-        
+
         new_address: int = self._address + offsets[0]
 
         for offset in offsets[1:]:
             new_address = self._process.read_ulonglong(new_address)
             new_address += offset
-        
+
         return MemoryPointer(
             self._process, self._module, new_address
         )
@@ -63,33 +64,33 @@ class MemoryPointer:
         return MemoryPointer(
             self._process, self._module, new_address
         )
-    
+
     def read_int(self) -> int:
         return self._process.read_int(self._address)
-    
+
     def read_string(self) -> str:
         return self._process.read_string(self._address)
-    
+
     def read_float(self) -> float:
         return self._process.read_float(self._address)
-    
+
     def read_bytes(self, size: int) -> bytes:
         return self._process.read_bytes(self._address, size)
-    
-    def write_int(self, value) -> None:
+
+    def write_int(self, value: int) -> None:
         self._process.write_int(self._address, value)
 
-    def write_float(self, value) -> None:
+    def write_float(self, value: float) -> None:
         self._process.write_float(self._address, value)
 
-    def write_string(self, value) -> None:
+    def write_string(self, value: str) -> None:
         self._process.write_string(self._address, value)
-    
-    def write_bytes(self, value, length) -> None:
+
+    def write_bytes(self, value: bytes, length: int) -> None:
         self._process.write_bytes(self._address, value, length)
-    
+
     def hex(self) -> str:
         return hex(self._address)
 
     def __int__(self) -> int:
-        return self.address
+        return self._address
